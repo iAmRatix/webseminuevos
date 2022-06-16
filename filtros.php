0125e1e0fc3f09a245bd06bc89a3fb5f53575c2e
iAmRatix <pccarrillop@gmail.com>
2022-06-15 19:12:01 -0500

    master

--- filtros.php
+++ filtros.php
@@ -79,7 +79,8 @@
           <div class="row">
 
           <?php
-          include 'conection.php';
+          $conn_string = "host=ec2-52-44-13-158.compute-1.amazonaws.com port=5432 dbname=d6i8s8b9suu22o user=wcmpzbduubrjor password=e4bc4d284593990d1c540b4f05bb867f12e1f1e052ea0acfafcdb13254fecbc4 connect_timeout='3000";
+          $conexion = pg_connect($conn_string);
 
           $marca=''; 
           $fecha = $_POST['fecha'];
