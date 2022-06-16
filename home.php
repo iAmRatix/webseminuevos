6b0252429794df18ded3740ec3cc1bc9ec307fdd
iAmRatix <pccarrillop@gmail.com>
2022-06-15 21:10:26 -0500

    master

--- home.php
+++ home.php
@@ -53,6 +53,9 @@
                               <li class="nav-item">
                                 <a class="nav-link" href="contactos.php">Contactos</a>
                               </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="cerrar.php">Cerrar Sesion</a>
+                              </li>
                               
                             </ul>
                           </div>
